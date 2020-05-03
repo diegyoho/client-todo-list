0d623e67dc810034e8d119c8afa3dcfd40f219b8
Diego Moreira <chicodiegomoreira@gmail.com>
2020-05-03 14:00:42 -0300

    Persist data on localstorage

--- docs/scripts.js
+++ docs/scripts.js
@@ -2,10 +2,16 @@ const listElement = document.querySelector('#todos ul')
 const addTodoInput = document.querySelector('#new-todo input')
 const addTodoButton = document.querySelector('#new-todo button')
 
+const todos = loadFromStorage()
+
+showList()
+
 addTodoButton.addEventListener('click', () => {
     if(addTodoInput.value !== '') {
-        listElement.appendChild(createItem(addTodoInput.value))
+        todos.push(addTodoInput.value)
         addTodoInput.value = ''
+        showList(todos)
+        saveToStorage()
     }
 })
 
@@ -14,4 +20,19 @@ function createItem(value) {
     li.innerText = value
 
     return li
+}
+
+function showList() {
+    listElement.innerHTML = ''
+
+    todos.forEach(todo => listElement.appendChild(createItem(todo)))
+}
+
+function loadFromStorage() {
+    const data = localStorage.getItem('todo-list') || '[]'
+    return JSON.parse(data)
+}
+
+function saveToStorage() {
+    localStorage.setItem('todo-list', JSON.stringify(todos))
 }
